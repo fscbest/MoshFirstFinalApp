0ca00a477dd867a1cd581d28839dc9caaed72b69
fscbest <romangeyber@gmail.com>
2016-07-04 18:54:56 +0300

    Project Initial

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+
+@Component({
+    selector: 'my-app',
+    templateUrl: '/app/app.component.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+export class AppComponent {
+}
