f2e2f6d77f278fe2a0b80faf866926216d77a906
fscbest <romangeyber@gmail.com>
2016-07-05 14:35:31 +0300

    Navigation Bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {NavBarComponent} from './navbar.component'
 
 
 @Component({
     selector: 'my-app',
     templateUrl: '/app/app.component.html',
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES, NavBarComponent]
 })
 export class AppComponent {
 }
