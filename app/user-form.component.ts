316cca539948cf623612716078ccb4651e8195db
fscbest <romangeyber@gmail.com>
2016-07-06 16:23:55 +0300

    Creating a user

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,19 +1,22 @@
-import {CanDeactivate} from "angular2/router";
+import {CanDeactivate, Router} from "angular2/router";
 import {Component, OnInit} from 'angular2/core';
 import {ControlGroup, FormBuilder} from 'angular2/common';
 import {Validators} from 'angular2/common';
 
 import {BasicValidators} from './basicValidators';
+import {UsersService} from './users.service';
 
 
 
 @Component({
-    templateUrl: '/app/user-form.template.html'
+    templateUrl: '/app/user-form.template.html',
+	providers: [UsersService]
+
 })
 export class UserFormComponent implements OnInit, CanDeactivate{
     form: ControlGroup;
 
-    constructor(private _fb: FormBuilder) {
+    constructor(private _fb: FormBuilder, private _router: Router, private _usersService: UsersService) {
 
 	}
 
@@ -41,5 +44,13 @@ export class UserFormComponent implements OnInit, CanDeactivate{
 		if(this.form.dirty){
 			return confirm("The form is changed. Are you sure?");
 		}
+		return true;
+	}
+
+	save(){
+		this._usersService.addUser(this.form.value)
+			.subscribe(x => {
+				this._router.navigate(['Users']);
+			});
 	}
 }
