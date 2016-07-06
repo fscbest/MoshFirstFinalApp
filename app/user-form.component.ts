44e7b3861a381eaa736a43c329536fafdc243bec
fscbest <romangeyber@gmail.com>
2016-07-06 15:00:10 +0300

    Move Validators to OnInit

--- app/user-form.component.ts
+++ app/user-form.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {ControlGroup, FormBuilder} from 'angular2/common';
 import {Validators} from 'angular2/common';
 
@@ -8,18 +8,22 @@ import {BasicValidators} from './basicValidators';
 @Component({
     templateUrl: '/app/user-form.template.html'
 })
-export class UserFormComponent{
+export class UserFormComponent implements OnInit{
     form: ControlGroup;
 
-    constructor(fb: FormBuilder) {
-		this.form = fb.group({
+    constructor(private _fb: FormBuilder) {
+
+	}
+
+	ngOnInit(){
+		this.form = this._fb.group({
 			name: ['', Validators.required],
 			email: ['', Validators.compose([
 				Validators.required,
 				BasicValidators.emailValidator
 			])],
 			phone: [],
-			address: fb.group({
+			address: this._fb.group({
 				street: [],
 				suite: [],
 				city: [],
