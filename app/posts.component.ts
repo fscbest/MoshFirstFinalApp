51e9c1c48edbb348ea6d25b46f119343c2fab5ef
fscbest <romangeyber@gmail.com>
2016-07-07 15:50:50 +0300

    Adding pagination

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from 'angular2/core';
 
 import {SpinnerComponent} from './spinner.component';
 
+import {PaginationComponent} from './pagination.component';
 import {PostsService} from './posts.service';
 import {UsersService} from './users.service';
 
@@ -9,7 +10,7 @@ import {UsersService} from './users.service';
 @Component({
     templateUrl: '/app/posts.template.html',
     providers: [PostsService, UsersService],
-    directives: [SpinnerComponent],
+    directives: [SpinnerComponent, PaginationComponent],
     styles: [`
         .posts	li	{	cursor:	default;	}
         .posts	li:hover	{	background:	#ecf0f1;	}
@@ -25,13 +26,14 @@ import {UsersService} from './users.service';
 export class PostsComponent implements OnInit {
     posts = [];
     users = [];
+    pagedPosts = [];
     currentPost;
     isPostsLoading = true;
     isCommentsLoading = true;
+    pageSize = 10;
 
-    constructor(
-        private _postsService:PostsService,
-        private _userService: UsersService) {
+    constructor(private _postsService:PostsService,
+                private _userService:UsersService) {
 
     }
 
@@ -40,37 +42,57 @@ export class PostsComponent implements OnInit {
         this.loadPosts();
     }
 
-    private loadUsers(){
+    private loadUsers() {
         this._userService.getUsers()
             .subscribe(users => this.users = users);
     }
 
-    private loadPosts(filter?){
+    private loadPosts(filter?) {
         this.isPostsLoading = true;
 
         this._postsService.getPosts(filter)
             .subscribe(
-                posts => this.posts = posts,
+                posts => {
+                    this.posts = posts;
+                    this.pagedPosts = this.getPostsInPage(1);
+                },
                 null,
                 () => {
                     this.isPostsLoading = false;
                 });
     }
 
-    reloadPosts(filter){
+    reloadPosts(filter) {
         this.currentPost = null;
 
         this.loadPosts(filter);
     }
 
-    select(post){
+    select(post) {
         this.currentPost = post;
         this.isCommentsLoading = true;
         this._postsService.getComments(post.id)
             .subscribe(
                 comments => this.currentPost.comments = comments,
                 null,
-                () => { this.isCommentsLoading = false;}
+                () => {
+                    this.isCommentsLoading = false;
+                }
             );
+    }
+
+    onPageChanged(page) {
+        this.pagedPosts = this.getPostsInPage(page);
+    }
+
+    private getPostsInPage(page) {
+        var result = [];
+        var startingIndex = (page - 1) * this.pageSize;
+        var endIndex = Math.min(startingIndex + this.pageSize, this.posts.length);
+
+        for (var i = startingIndex; i < endIndex; i++)
+            result.push(this.posts[i]);
+
+        return result;
     }
 }
