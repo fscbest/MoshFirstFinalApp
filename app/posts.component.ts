6499b075879a21cbf1cae7b3f0d306652f6a922c
fscbest <romangeyber@gmail.com>
2016-07-07 16:13:34 +0300

    Add UNDERSCORE pagination

--- app/posts.component.ts
+++ app/posts.component.ts
@@ -54,7 +54,7 @@ export class PostsComponent implements OnInit {
             .subscribe(
                 posts => {
                     this.posts = posts;
-                    this.pagedPosts = this.getPostsInPage(1);
+                    this.pagedPosts = _.take(this.posts, this.pageSize); //this.getPostsInPage(1);
                 },
                 null,
                 () => {
@@ -82,17 +82,20 @@ export class PostsComponent implements OnInit {
     }
 
     onPageChanged(page) {
-        this.pagedPosts = this.getPostsInPage(page);
+        var startIndex = (page - 1) * this.pageSize;
+
+        //this.pagedPosts = this.getPostsInPage(page);
+        this.pagedPosts = _.take(_.rest(this.posts, startIndex), this.pageSize)
     }
 
-    private getPostsInPage(page) {
-        var result = [];
-        var startingIndex = (page - 1) * this.pageSize;
-        var endIndex = Math.min(startingIndex + this.pageSize, this.posts.length);
-
-        for (var i = startingIndex; i < endIndex; i++)
-            result.push(this.posts[i]);
-
-        return result;
-    }
+    //private getPostsInPage(page) {
+    //    var result = [];
+    //    var startingIndex = (page - 1) * this.pageSize;
+    //    var endIndex = Math.min(startingIndex + this.pageSize, this.posts.length);
+    //
+    //    for (var i = startingIndex; i < endIndex; i++)
+    //        result.push(this.posts[i]);
+    //
+    //    return result;
+    //}
 }
